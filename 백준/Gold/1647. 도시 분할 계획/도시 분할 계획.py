4024bf507e01ce8ddcbb35d8c9884f4da0b43afc
chxghee <159872190+chxghee@users.noreply.github.com>
2025-03-05 12:21:58 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3628 ms, Memory: 199932 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+
+parent = [0] * (n+1)
+for i in range(1, n+1):
+    parent[i] = i
+
+edges = []
+min_cost = 0
+cost_list = []
+
+for _ in range(m):
+    a, b, w = map(int, input().split())
+    edges.append((w,a,b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a,b)
+        cost_list.append(cost)
+
+result = sum(cost_list) - max(cost_list)
+print(result)

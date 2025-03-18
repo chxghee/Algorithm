6c6af08b1519f8066b02bdffaecb131ac907bb7d
chxghee <francis001021@gmail.com>
2025-03-18 23:15:18 +0900

    Add: 그리디 알고리즘 연습문제 sol 추가

---
+++ 이것이코딩테스트다/10장_그래프이론/연습문제/45최종순위.py
@@ -0,0 +1,87 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def make_graph(t, change):
+
+    graph = [[False] *(n+1) for _ in range(n+1)]
+    indegree = [0] * (n+1)
+    for i in range(n):
+        for j in range(i+1, n):
+            graph[t[i]][t[j]] = True
+            indegree[t[j]] += 1
+    
+    for val in change:
+        x, y = val
+        if graph[x][y]:
+            graph[x][y] = False
+            graph[y][x] = True
+            indegree[x] += 1
+            indegree[y] -= 1
+        else:
+            graph[x][y] = True
+            graph[y][x] = False
+            indegree[x] -= 1
+            indegree[y] += 1
+            
+    return graph, indegree
+
+
+def topology_sort(graph, indegree):
+    result = []
+    q = deque()
+
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+    
+    
+    cycle = False
+    ambi = False
+
+    if not q:
+        cycle = True
+
+    while q:
+     
+        if len(q) >= 2: # 순서 모호
+            ambi = True
+            break
+        
+        node = q.popleft()
+        result.append(node)
+
+        for i in range(1, n+1):
+            if graph[node][i]:
+                indegree[i] -= 1
+
+                if indegree[i] == 0:
+                    q.append(i)
+
+    
+    if cycle or len(result) != n:
+        print("IMPOSSIBLE")
+    elif ambi:
+        print("?")
+    else:
+        for val in result:
+            print(val, end=" ")
+        print()
+        
+
+
+
+a = int(input())
+
+for _ in range(a):
+    n = int(input())
+    t = list(map(int, input().split()))
+    m = int(input())
+    change = []
+    for _ in range(m):
+        x, y = map(int, input().split())
+        change.append((x, y))
+    graph, indegree = make_graph(t, change)
+    topology_sort(graph, indegree)
+    
+
---
+++ 이것이코딩테스트다/유형별_연습문제/그리디/수들의합.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1789
+import sys
+input = sys.stdin.readline
+
+s = int(input())
+sum_result = 0
+cnt = 0
+i = 1
+
+while sum_result < s:
+    sum_result += i
+    i += 1
+
+if sum_result != s:
+    i -= 1
+
+
+print(i - 1)
+

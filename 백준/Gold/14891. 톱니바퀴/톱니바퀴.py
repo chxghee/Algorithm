a8a0eef4f7b6eb66a07366f68b9e9cf574026c51
chxghee <159872190+chxghee@users.noreply.github.com>
2025-04-03 22:00:41 +0900

    [Gold V] Title: 톱니바퀴, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,54 +1,80 @@
+# https://www.acmicpc.net/problem/14891
+
+# 같으면 회전 x 다르면  반대 방향 회전  / N = 0, S = 1 / 1 시계, -1 반시계(앞에꺼 빼서 뒤에)
+
 import sys
 from collections import deque
 input = sys.stdin.readline
 
+# 2 - 6 
+
+
 def turn_cog(turn_cog_num, dirt):
-    turn_cog_num -= 1  # 입력이 1-based이므로 0-based로 변환
+    turn_cog_num -= 1
 
-    turn = [0] * 4  # 각 톱니바퀴의 회전 방향 저장 (0: 회전 안 함, 1: 시계, -1: 반시계)
-    turn[turn_cog_num] = dirt  # 현재 톱니바퀴 회전 방향 설정
-
-    # 오른쪽으로 전파
-    d = dirt  # 현재 회전 방향
-    for i in range(turn_cog_num, 3):  # 현재 톱니부터 오른쪽 끝까지 검사
-        right = i + 1  # 오른쪽 톱니
-        if cog[i][2] != cog[right][6]:  # 맞닿은 부분(2,6) 비교
-            d *= -1  # 반대 방향으로 회전
+    turn = [0] * 4
+    turn[turn_cog_num] = dirt
+   
+   
+    d = dirt
+    for i in range(turn_cog_num, 3):
+        right = 1 + i
+        
+        if cog[right][6] != cog[i][2]:
+            d *= -1
             turn[right] = d
         else:
-            break  # 같으면 전파 멈춤
-
-    # 왼쪽으로 전파
-    d = dirt
-    for i in range(turn_cog_num, 0, -1):  # 현재 톱니부터 왼쪽 끝까지 검사
-        left = i - 1  # 왼쪽 톱니
-        if cog[i][6] != cog[left][2]:  # 맞닿은 부분(6,2) 비교
-            d *= -1  # 반대 방향으로 회전
+            break
+        
+               
+    d = dirt 
+    for i in range(turn_cog_num, 0, -1):
+        left = i - 1
+        
+        if cog[left][2] != cog[i][6]:
+            d *= -1
             turn[left] = d
         else:
-            break  # 같으면 전파 멈춤
+            break
+         
 
-    # 실제 회전 수행
     for i in range(4):
-        if turn[i] == 1:    # 시계 방향 회전
-            cog[i].appendleft(cog[i].pop())
-        elif turn[i] == -1:  # 반시계 방향 회전
-            cog[i].append(cog[i].popleft())
+        if turn[i] == -1:
+            data = cog[i].popleft()
+            cog[i].append(data)
+        
+        if turn[i] == 1:
+            data = cog[i].pop()
+            cog[i].appendleft(data)
 
-# 입력 받기
-cog = [deque(map(int, input().strip())) for _ in range(4)]
+
+
+
+cog = [deque(list(map(int, input().rstrip()))) for _ in range(4)]
+
+
+
 k = int(input())
 
 for _ in range(k):
     cog_num, dirt = map(int, input().split())
     turn_cog(cog_num, dirt)
 
-# 결과 계산
+
 result = 0
-point = [1, 2, 4, 8]
+point = {
+    0: 1,
+    1: 2,
+    2: 4,
+    3: 8 
+}
+
 
 for i in range(4):
     if cog[i][0] == 1:
         result += point[i]
 
 print(result)
+
+
+

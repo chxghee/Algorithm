cbe1dd8d95b684a3e68399822c04dcebd9d7416c
chxghee <159872190+chxghee@users.noreply.github.com>
2025-04-16 00:27:49 +0900

    [Gold V] Title: 보물섬, Time: 4712 ms, Memory: 34984 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -0,0 +1,43 @@
+# https://www.acmicpc.net/problem/2589
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    distance = [[-1] * m for _ in range(n)]
+    distance[x][y] = 0
+    while q:
+        nx, ny = q.popleft()
+
+        for i in range(4):
+            cx = nx + dx[i]
+            cy = ny + dy[i]
+
+            if 0 <= cx < n and 0 <= cy < m and data[cx][cy] == 'L' and distance[cx][cy] == -1:
+                distance[cx][cy] = distance[nx][ny] + 1
+                q.append((cx,cy))
+
+    result = 0
+    for val in distance:
+        md = max(val)
+        result = max(result, md)
+
+    return result
+
+
+n, m = map(int,input().split())
+data = [list(input().rstrip()) for _ in range(n)]
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if data[i][j] == 'L':
+            dist = bfs(i,j)
+            result = max(result, dist)
+
+print(result)

a123ce0ae96419c87297961a31f4a7576db5d06c
chxghee <159872190+chxghee@users.noreply.github.com>
2025-04-23 11:27:30 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 972 ms, Memory: 119700 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/10816
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+n = int(input())
+has = list(map(int, input().split()))
+
+m = int(input())
+data = list(map(int, input().split()))
+
+find = [0] * m
+
+has.sort()
+has_cnt = Counter(has)
+
+for i in range(m):
+    find[i] = has_cnt[data[i]]
+
+for val in find:
+    print(val, end=" ")

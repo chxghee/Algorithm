e75e40c038c15736d9ae97d907451fde787e4d5a
chxghee <159872190+chxghee@users.noreply.github.com>
2025-06-23 11:45:03 +0900

    [Silver I] Title: 경로 찾기, Time: 104 ms, Memory: 35076 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,43 @@
+# https://www.acmicpc.net/problem/11403
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def find_adj(q, node):
+    
+    if not q:
+        return
+    
+    adj_list = set(q)
+    
+    while q:
+        adj = q.popleft()
+        for k in range(n):
+            if matrix[adj][k] == 1:
+                if k not in adj_list:
+                    adj_list.add(k)
+                    q.append(k)
+
+    for val in adj_list:
+        matrix[node][val] = 1
+
+
+
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+
+for i in range(n):
+    q = deque()
+    for j in range(n):    
+        if matrix[i][j] == 1:
+            q.append(j)
+    find_adj(q, i)
+
+
+for i in range(n):
+    for j in range(n):
+        print(matrix[i][j], end=' ')
+    print()

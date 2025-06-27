3e2f69b6fe0360d00c43291e9b13f77daa42755b
chxghee <159872190+chxghee@users.noreply.github.com>
2025-06-27 14:56:52 +0900

    [Gold III] Title: 파티, Time: 52 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,18 +1,15 @@
 # https://www.acmicpc.net/problem/1238
+# 다익스트라는 특정 출발지부터 다른 모든 노드의 최단 거리를 구할 수 있다.
+# -> 그럼 어떤 목적지가 있을때 모든 노드에서 이 목적지로의 최단 거리를 구하려면?
+# -> 그래프를 뒤집은뒤 최단 거리를 구하면 상당히 효율적으로 구할 수 있다.
+
 import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, m, dst = map(int, input().split())
 
-graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    a, b, w = map(int, input().split())
-    graph[a].append((b, w))
-
-
-def dijckstra(start, end):
+def dijckstra(start, graph):
 
     distance = [INF] * (n+1)
 
@@ -32,11 +29,21 @@ def dijckstra(start, end):
                 distance[adj[0]] = cost
                 heapq.heappush(q, (cost, adj[0]))
     
-    return distance[end]
+    return distance
+
+n, m, dst = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+reversed_graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, w = map(int, input().split())
+    graph[a].append((b, w))
+    reversed_graph[b].append((a, w))
+
 
 result = [0] * (n+1)
 
-for i in range(1, n+1):
-    result[i] = dijckstra(i, dst) + dijckstra(dst, i)
+go_party = dijckstra(dst, reversed_graph)
+go_home = dijckstra(dst, graph)
 
-print(max(result))
+print(max(map(sum, zip(go_party[1:], go_home[1:]))))

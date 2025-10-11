77b7d1aac0e68de45164e2d1896310878bd7ec63
Changhee Lee <159872190+chxghee@users.noreply.github.com>
2025-10-11 21:26:13 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 240 ms, Memory: 25636 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Set<String> participants = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+
+            if (str.equals("ENTER")) {
+                participants.clear();
+                continue;
+            }
+            if (participants.add(str)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
